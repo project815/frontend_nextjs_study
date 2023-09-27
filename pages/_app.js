ac10124f5f23f5241e97b98ebc7f92d729a18fdf
songminseok <ske3056@gmail.com>
2023-09-27 20:32:53 +0900

    feat: graphql

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-// import '@/styles/globals.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const client = new ApolloClient({
+    uri: "http://practice.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
