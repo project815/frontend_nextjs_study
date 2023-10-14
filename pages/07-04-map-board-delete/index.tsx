29eedbb42ca282a0c68bea4a5a73f41e8c598ae2
songminseok <ske3056@gmail.com>
2023-10-14 17:43:13 +0900

    feat: delete board

--- pages/07-04-map-board-delete/index.tsx
+++ pages/07-04-map-board-delete/index.tsx
@@ -1,5 +1,6 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
 
 const FETCHBOARDS = gql`
   query fetchBoards {
@@ -12,24 +13,60 @@ const FETCHBOARDS = gql`
   }
 `;
 
+const DELETEBOARD = gql`
+  mutation deleteBoard($number: Int) {
+    deleteBoard(number: $number) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
 const Row = styled.div`
   display: flex;
 `;
 const Column = styled.div`
   width: 20%;
 `;
+const Button = styled.button`
+  font-weight: bold;
+`;
 export default function StaticRoutePage() {
   const { data } = useQuery(FETCHBOARDS);
+  const [deleteBoard] = useMutation(DELETEBOARD);
 
-  console.log("data :", data);
-  console.log("data?.fetchBoards :", data?.fetchBoards);
+  const onClickDeleteBoard = async (num: { num: Number }) => {
+    console.log(num);
+    await deleteBoard({
+      variables: { number: num },
+      refetchQueries: [{ query: FETCHBOARDS }],
+    }).catch((err) => {
+      console.log("err : ", err);
+    });
+  };
+
+  console.log("실행");
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <>
       {data &&
         data?.fetchBoards.map((ele) => (
-          <Row>
+          <Row key={ele.number}>
+            <Column>
+              <input
+                type="checkbox"
+                onChange={(event) => (ele.checked = event.target.checked)}
+              />
+            </Column>
             <Column>{ele.number}</Column>
             <Column>{ele.title}</Column>
+            <Column>{ele.contents}</Column>
+            <Button onClick={() => onClickDeleteBoard(ele.number)}>삭제</Button>
           </Row>
         ))}
     </>
