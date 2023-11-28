987df9ca7ae6bc55c4240b508f45cab02a7028f2
songminseok <ske3056@gmail.com>
2023-11-29 00:53:24 +0900

    feat: 02-02-signup-state

---
+++ pages/section02/02-02-signup-state/index.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEvent, useState } from "react";
+
+export default function SignUpStatePage() {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const [emailError, setEmailError] = useState<string>("");
+
+  const onClickkSignUp = () => {
+    if (!email.includes("@")) {
+      setEmailError("잘못된 형식의 이메일입니다.");
+    } else {
+      setEmailError("");
+      alert("회원가입이 완료되었습니다.");
+    }
+  };
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("e : ", e);
+    console.log("e : ", e.target);
+    console.log("e : ", e.target.value);
+    setEmail(e.target.value);
+  };
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  return (
+    <div>
+      이메일: <input type="text" onChange={onChangeEmail} />
+      <div>{emailError}</div>
+      비밀번호: <input type="password" onChange={onChangePassword} />
+      <button onClick={onClickkSignUp}>회원가입</button>
+    </div>
+  );
+}
