d78e0963c1ea8371d5f76dc27a964bbb72ca61ce
songminseok <ske3056@gmail.com>
2023-12-08 16:40:58 +0900

    feat: setState 원리

--- src/components/units/board/section07/BoardWrite.container.tsx
+++ src/components/units/board/section07/BoardWrite.container.tsx
@@ -26,22 +26,29 @@ export default function BoardWrite() {
   const onChangeWriter = (e: React.ChangeEvent<HTMLInputElement>) => {
     setWriter(e.target.value);
     console.log("writer : ", writer);
+    e.target.value && title && contents
+      ? setIsActive(true)
+      : setIsActive(false);
   };
   const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
+    writer && e.target.value && contents
+      ? setIsActive(true)
+      : setIsActive(false);
   };
   const onChangeContents = (e: React.ChangeEvent<HTMLInputElement>) => {
     setContents(e.target.value);
+    writer && title && e.target.value ? setIsActive(true) : setIsActive(false);
   };
 
-  useEffect(() => {
-    console.log(`writer : ${writer}, title : ${title}, contents : ${contents}`);
-    if (writer && title && contents) {
-      setIsActive(true);
-    } else {
-      setIsActive(false);
-    }
-  }, [writer, title, contents]);
+  // useEffect(() => {
+  //   console.log(`writer : ${writer}, title : ${title}, contents : ${contents}`);
+  //   if (writer && title && contents) {
+  //     setIsActive(true);
+  //   } else {
+  //     setIsActive(false);
+  //   }
+  // }, [writer, title, contents]);
 
   return (
     <BoardWriteUI
