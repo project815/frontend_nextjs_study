73d5083a08afc3cb5e199f8c3dac70e375367d4c
songminseok <ske3056@gmail.com>
2023-12-08 17:14:51 +0900

    feat:section08-02-map-boards

---
+++ pages/section08/08-02-map-boards/index.tsx
@@ -0,0 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
+
+const FETCHBOARDS = gql`
+  query {
+    fetchBoards {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+export default function MapBoardsPage() {
+  const { data } = useQuery(FETCHBOARDS);
+  console.log("data : ", data);
+  return (
+    <div>
+      {data &&
+        data?.fetchBoards.map(
+          (i: { writer: string; contents: string; title: string; number }) => (
+            <div key={i.number} style={{ marginTop: "10px" }}>
+              <span style={{ marginLeft: "4px" }}>
+                <input type="checkbox" />
+              </span>
+              <span>{i.number}</span>
+              <span style={{ marginLeft: "4px" }}>
+                작성자 : {i && i.writer}
+              </span>
+              <span style={{ marginLeft: "4px" }}>제목 : {i.title}</span>
+              <span style={{ marginLeft: "4px" }}>
+                내용 : {i ? i.contents : ""}
+              </span>
+            </div>
+          )
+        )}
+    </div>
+  );
+}
