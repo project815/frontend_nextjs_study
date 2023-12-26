605a533c4588637c7dc234db891bfeb0b523b9b9
songminseok <ske3056@gmail.com>
2023-12-26 15:53:48 +0900

    feat: 16-01 edit02: 스프래드 연산자, 원본을 그대로 두고 복사해서 바꿔서 쓰자

--- pages/section16/16-01-comment-edit-02/index.tsx
+++ pages/section16/16-01-comment-edit-02/index.tsx
@@ -16,17 +16,25 @@ const FETCHBOARDS = gql`
 export default function MapBoardsPage() {
   const { data } = useQuery<Pick<IQuery, "fetchBoards">>(FETCHBOARDS);
 
-  const [selectedIdx, setSelectedIdx] = useState<number>(-1);
+  const [selectedIdx, setSelectedIdx] = useState<boolean[]>(
+    new Array(10).fill(false)
+  );
 
   const onClickEdit = (e: React.MouseEvent<HTMLButtonElement>): void => {
-    const result = Number(e.currentTarget.id);
-    setSelectedIdx(result);
+    // const qqq = selectedIdx; // 배열은 주소값 복사가 이루어짐,  qqq가 변경되면 selectedIdx의 값도 변경함. setState를 하더라도 값이 동일함으로 동작하지 않음.
+    const qqq = [...selectedIdx]; // 배열을 스프레드 연산자로 복사, 주소값이 달라 qqq가 변경되어도 selectIdx가 변경되지 않음 setState가 동작함.
+    console.log("selectedIdx : ", selectedIdx);
+    qqq[Number(e.currentTarget.id)] = true;
+
+    setSelectedIdx(qqq);
   };
+
+  console.log("selectedIdx : ", selectedIdx);
   return (
     <div>
       <div>
         {data?.fetchBoards.map((i, index) =>
-          index !== selectedIdx ? (
+          !selectedIdx[index] ? (
             <div key={i._id} style={{ marginTop: "10px" }}>
               <span style={{ marginLeft: "4px" }}>
                 내용 : {i ? i.contents : "불러오는 중"}
